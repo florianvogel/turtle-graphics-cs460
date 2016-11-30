e50acef8dfa7cc9a560b38871beea0c427521543
jayden626 <jayden_626@hotmail.com>
2016-11-30 14:24:23 -0700

    Implemented canvas

---
+++ server/TurtleCanvas.java
@@ -0,0 +1,57 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+public class TurtleCanvas extends Canvas{
+
+	private Canvas canvas;
+	private int x, y; //The current pen coordinates
+	private int posX, posY; //The position to move the pen to
+	private boolean penDown;
+	private int length;
+
+	public TurtleCanvas(){
+		canvas = new Canvas();
+		posX = 0;
+		posY = 0;
+		penDown = false;
+		length = 0;
+	}
+
+	@Override
+	public void paint(Graphics g){
+		g.drawLine(x,y,posX,posY);
+	}
+
+	//Getters and Setters
+	public void setPosX(int newX){
+		posX = newX;
+	}
+
+	public void setPosY(int newY){
+		posY = newY;
+	}
+
+	public void setLength(int newLength){
+		length = newLength;
+	}
+
+	public void setPenDown(boolean newPenDown){
+		penDown = newPenDown;
+	}
+
+	public int getPosX(){
+		return posX;
+	}
+
+	public int getPosY(){
+		return posY;
+	}
+
+	public int getLength(){
+		return length;
+	}
+
+	public boolean isPenDown(){
+		return penDown;
+	}
+}
